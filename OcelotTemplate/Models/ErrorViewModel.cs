38d5ac50374943728303de84eafb0a100a4c6dda
murtozayev jahongir <mirjahongirbek@gmail.com>
2019-02-13 15:34:57 +0500

    Add project files.

---
+++ OcelotTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OcelotTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
